4f5a2cbf0f9f025fc20e39d1b4bfaebb8456cc22
rithiksai27 <rithiksai27@gmail.com>
2024-10-17 21:05:41 +0530

    commit-10

--- index.js
+++ index.js
@@ -6,22 +6,18 @@ const authRoute = require("./routes/auth");
 const userRoute = require("./routes/users");
 const postRoute = require("./routes/posts");
 const categoryRoute = require("./routes/categories");
-const cors  = require('cors');
+const cors = require('cors');
 const multer = require("multer");
 const path = require("path");
 const PORT = 3002;
+
 dotenv.config();
 app.use(express.json());
 app.use("/images", express.static(path.join(__dirname, "/images")));
-
-
 app.use(cors());
 
 mongoose
-  .connect(process.env.MONGO_URL, {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-  })
+  .connect(process.env.MONGO_URL) // Removed useNewUrlParser and useUnifiedTopology
   .then(() => {
     console.log("Connected to MongoDB");
   })
@@ -43,7 +39,6 @@ app.post("/api/upload", upload.single("file"), (req, res) => {
   res.status(200).json("File has been uploaded");
 });
 
-
 app.use("/api/auth", authRoute);
 app.use("/api/users", userRoute);
 app.use("/api/posts", postRoute);
@@ -52,4 +47,3 @@ app.use("/api/categories", categoryRoute);
 app.listen(PORT, () => {
   console.log(`Server is running on PORT ${PORT}`);
 });
-
